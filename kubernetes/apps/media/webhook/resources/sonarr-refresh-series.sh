91a900aa311d5dcc68d5178c3b4311a35e55f56d
Nicole Hubbard <code@nicole.dev>
2025-08-26 18:42:53 -0500

    fix envvar name

--- kubernetes/apps/media/webhook/resources/sonarr-refresh-series.sh
+++ kubernetes/apps/media/webhook/resources/sonarr-refresh-series.sh
@@ -9,7 +9,7 @@ function refresh() {
         echo "[DEBUG] test event received from ${SONARR_REMOTE_ADDR}, nothing to do ..."
     elif [[ "${SONARR_EVENT_TYPE}" == "Grab" ]]; then
         episodes=$(
-            curl -fsSL --header "X-Api-Key: ${SONARR_API_KEY}" "http://${SONARR_REMOTE_ADDR}/api/v3/episode?seriesId=${SERIES_ID}" |
+            curl -fsSL --header "X-Api-Key: ${SONARR_API_KEY}" "http://${SONARR_REMOTE_ADDR}/api/v3/episode?seriesId=${SONARR_SERIES_ID}" |
                 jq --raw-output '[.[] | select((.title == "TBA") or (.title == "TBD"))] | length'
         )
         if ((episodes > 0)); then
@@ -17,7 +17,7 @@ function refresh() {
             curl -fsSL --request POST \
                 --header "X-Api-Key: ${SONARR_API_KEY}" \
                 --header "Content-Type: application/json" \
-                --data-binary "$(jo name=RefreshSeries seriesId="${SERIES_ID}")" \
+                --data-binary "$(jo name=RefreshSeries seriesId="${SONARR_SERIES_ID}")" \
                 "http://${SONARR_REMOTE_ADDR}/api/v3/command" &>/dev/null
         fi
     fi
